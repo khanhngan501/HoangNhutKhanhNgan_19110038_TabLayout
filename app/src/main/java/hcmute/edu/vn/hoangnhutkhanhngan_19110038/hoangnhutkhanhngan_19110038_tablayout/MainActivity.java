342b73b45bace43ac55d80da7195f6451b1e5ea3
khanhngan501 <khanhngan501@gmail.com>
2022-05-09 23:14:05 +0700

    Initial commit

---
+++ app/src/main/java/hcmute/edu/vn/hoangnhutkhanhngan_19110038/hoangnhutkhanhngan_19110038_tablayout/MainActivity.java
@@ -0,0 +1,30 @@
+package hcmute.edu.vn.hoangnhutkhanhngan_19110038.hoangnhutkhanhngan_19110038_tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabLayout = findViewById(R.id.tab_layout);
+        mViewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
